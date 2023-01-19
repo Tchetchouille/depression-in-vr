95e3ebd680bdcda3bdbccfe379c69970b12a40af
Tchetchouille <Tchetchouille@users.noreply.github.com>
2023-01-19 18:41:39 +0100

    Texture rework + Added laptop

--- components/sunset.js
+++ components/sunset.js
@@ -5,9 +5,9 @@ AFRAME.registerComponent('sunset', {
         let aDuration =   10000;
         
         if(this.el.id == "sunlight"){
-            this.el.setAttribute('animation__intensity', {property: "light.intensity", to: 0.6, dur: aDuration, loop: true, easing: "linear", dir: "alternate"});
+            this.el.setAttribute('animation__intensity', {property: "light.intensity", to: 0.4, dur: aDuration, loop: true, easing: "linear", dir: "alternate"});
             this.el.setAttribute('animation__color', {property: "light.color", to: "#f97247", dur: aDuration, loop: true, easing: "easeInQuad", dir: "alternate"});
-            this.el.parentEl.setAttribute('animation__rotation', {property: "rotation", to: {x: 60, y: 0, z: 0}, dur: aDuration, loop: true, easing: "linear", dir: "alternate"});
+            this.el.parentEl.setAttribute('animation__rotation', {property: "rotation", to: {x: 75, y: -25, z: 0}, dur: aDuration, loop: true, easing: "linear", dir: "alternate"});
         }
         else{
             this.el.setAttribute('animation', {property: "light.intensity", to: 0, dur: aDuration, loop: true, easing: "linear", dir: "alternate"});
